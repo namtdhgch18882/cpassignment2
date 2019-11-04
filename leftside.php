c740defbfadb54e40d9158b63c42448ad6e5f8f4
namtdhgch18882 <55873400+namtdhgch18882@users.noreply.github.com>
2019-11-04 18:04:14 +0700

    Create leftside.php

---
+++ leftside.php
@@ -0,0 +1,8 @@
+<div class = "advert">
+<ul class="sidebar">
+	<li><a class="active" href="#">DEPARTMENT</a></li>
+	<li><a href="productbycat.php?CatId=1">Electronics</a></li>
+	<li><a href="productbycat.php?CatId=2">Home & Kitchen</a></li>
+	<li><a href="productbycat.php?CatId=3">Music, CDs & Vinyl</a></li>
+	<li><a href="productbycat.php?CatId=4">Movies & Television</a></li>
+</ul>
