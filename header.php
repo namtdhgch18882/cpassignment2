bd5706cf4b401a853def5968bd948a043e65062f
namtdhgch18882 <55873400+namtdhgch18882@users.noreply.github.com>
2019-11-04 18:04:42 +0700

    Create header.php

---
+++ header.php
@@ -0,0 +1,9 @@
+<div class= "header">
+<div class="inner-header">
+<img class="logo" src="img/logo.png">
+<ul class="navbar">
+  <li><a href="index.php">Home</a></li>
+  <li><a href="aboutpage.php">About</a></li>
+  <li><a href="login.php">Login</a></li>
+</ul>
+</div>
