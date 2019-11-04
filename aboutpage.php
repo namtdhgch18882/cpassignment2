f37239bf8f9402df91c40da1aabbb00911bd9765
namtdhgch18882 <55873400+namtdhgch18882@users.noreply.github.com>
2019-11-04 18:04:25 +0700

    Create aboutpage.php

---
+++ aboutpage.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>About Us</title>
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<?php require_once("./header.php"); ?>
+	
+	<div class = "about-section">
+	
+	<h2> About us </h2>
+	
+	<span class ="border"></span>
+	
+	<p> Our company provides shipping services and is widely recognized internationally. We deliver packages to our partners storage facilities, from where they can be shipped to any place worldwide by any means of transport.</p>
+	<br>
+	<p> Our motto is “Crossing borders without barriers”. We are able to deliver anything to any country or place in the world. People and organizations that are our regular customers are located in such countries as Australia, Canada, Chile, Japan, Poland, Hungary, Russia and many more.</p>
+	<br>
+	<p>Our company is highly experienced in delivering packages and does so in compliance with respective shipping laws of a large number of countries worldwide. </p>
+	<br>
+	<p> As our main principles we see safety, reliability, transparent price policy, responsibility, loyalty and customer attention. The company’s managers pay special attention to every client’s needs and requests, and act according to them. We always have the right option for every customer.</p>
+	<br>
+	Etrinxy works in the benefit of its clients and contributes to making the shopping process more favorable.
+
+	</div>
+	
+	<?php require_once("./footer.php"); ?>
