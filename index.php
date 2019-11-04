e78f3279fbb51ee3a51c08632c1c5c7f3e7ac876
namtdhgch18882 <55873400+namtdhgch18882@users.noreply.github.com>
2019-11-04 18:04:11 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Homepage</title>
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<?php require_once("./header.php"); ?>
+	<div class="advert">
+	<?php require_once("./leftside.php"); ?>
+	<div class = "content"> 
+		<img src="img/moreadvert.jpg" style="width:100%; height:100%;">
+	</div>
+	<?php require_once("./footer.php"); ?>
