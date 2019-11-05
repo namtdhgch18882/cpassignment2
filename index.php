5744c65d31817dd89c451ce9ddbdb1c595fc0b10
namtdhgch18882 <55873400+namtdhgch18882@users.noreply.github.com>
2019-11-05 09:15:30 +0700

    Update index.php

--- index.php
+++ index.php
@@ -9,6 +9,6 @@
 	<div class="advert">
 	<?php require_once("./leftside.php"); ?>
 	<div class = "content"> 
-		<img src="img/moreadvert.jpg" style="width:100%; height:100%;">
+		<img src="../img/moreadvert.jpg" style="width:100%; height:100%;">
 	</div>
 	<?php require_once("./footer.php"); ?>
