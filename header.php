e462248679ac44913262e8f0fc0675191ef3d717
namtdhgch18882 <55873400+namtdhgch18882@users.noreply.github.com>
2019-11-05 09:24:48 +0700

    Update header.php

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <div class= "header">
 <div class="inner-header">
-<img class="logo" src="../img/logo.png">
+<img class="logo" src="img/logo.png">
 <ul class="navbar">
   <li><a href="index.php">Home</a></li>
   <li><a href="aboutpage.php">About</a></li>
